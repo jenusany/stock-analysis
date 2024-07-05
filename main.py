85a347dcaa53a589f663439d6f4b906c64a83a2f
jenusany <jenusany@outlook.com>
2024-07-05 16:31:04 -0400

    stock ticker can be selected and appropriate data can be visualized to user

--- main.py
+++ main.py
@@ -4,3 +4,36 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+
+START = '2015-01-01'
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediciton App")
+
+stocks = ("AAPL", "GOOG")
+selected_stocks = st.selectbox("Select dataset for prediciton", stocks)
+
+n_years = st.slider("Years of prediciton:", 1,4)
+period = n_years * 365
+
+@st.cache_data
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stocks)
+data_load_state.text("Done")
+
+st.subheader("Raw data")
+st.write(data.tail(5))
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Open'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+    
+plot_raw_data()
+
